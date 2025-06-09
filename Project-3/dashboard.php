c05744955b8e1cbacbe46888cb3a791dfc6a1ef3
Surya K <rajsurya5789@gmail.com>
2025-06-09 19:30:05 +0530

    Delete Project-3 directory

--- Project-3/dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION["user_id"])) {
-    header("Location: index.html");
-    exit();
-}
-
-echo "Welcome to your dashboard! <a href='logout.php'>Logout</a>";
-?>
